6457c262f268c35de4c0c8ebf1e4fc4a808de528
Tamara <tylues@uc.cl>
2022-03-11 15:25:45 -0300

    feat(): add inline-svg

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import Vue from 'vue/dist/vue.esm';
-import App from '../app.vue';
+import InlineSvg from 'vue-inline-svg';
 import '../css/application.css';
+
+
+Vue.component('inline-svg', InlineSvg);
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
