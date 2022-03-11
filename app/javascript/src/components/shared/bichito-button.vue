678f9a2117c226e022c1b5f2c2c52a4ab6739b51
Tamara <tylues@uc.cl>
2022-03-11 15:28:03 -0300

    feat(button): addd bichito button

---
+++ app/javascript/src/components/shared/bichito-button.vue
@@ -0,0 +1,21 @@
+<template>
+  <component
+    :is="tag"
+    class="p-4 text-sm font-bold text-white bg-gray-800 rounded-full sm:text-base sm:px-6 hover:shadow-lg hover:bg-gray-700"
+    @click="emit('click')"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  computed: {
+    tag() {
+      if (this.alternativeTag) return this.alternativeTag;
+
+      return this.$attrs.href ? 'a' : 'button';
+    },
+  },
+};
+</script>
