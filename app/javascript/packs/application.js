f4f058a27ab8ab89399a00a0230634fb2ebd7ba4
Tamara <tylues@uc.cl>
2022-03-11 15:29:11 -0300

    feat(application): add components to application js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,19 @@ import Vue from 'vue/dist/vue.esm';
 import InlineSvg from 'vue-inline-svg';
 import '../css/application.css';
 
+import LandingHero from '../src/components/landing/landing-hero.vue';
+import LandingNavbar from '../src/components/landing/landing-navbar.vue';
 
 Vue.component('inline-svg', InlineSvg);
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#vue-app',
+    components: {
+      LandingHero,
+      LandingNavbar,
     components: { App },
+    },
   });
 
   return app;
