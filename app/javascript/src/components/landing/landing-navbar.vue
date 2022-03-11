70bb9239f8259c644bba7305b67d1225999b2cd5
Tamara <tylues@uc.cl>
2022-03-11 15:27:48 -0300

    feat(navbar): add navbar component

---
+++ app/javascript/src/components/landing/landing-navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav
+    class="absolute top-0 left-0 right-0 z-10 items-center py-5"
+    :class="isMenuOpen ? 'bg-sc-gray-darker': 'bg-sc-blue' "
+  >
+    <inline-svg
+      :src="require('../../../../assets/images/icons/bichito.svg')"
+      class="text-gray-700 w-24 lg:w-32 mx-10 md:mx-32 lg:mx-56"
+      alt="Logo Bichito"
+    />
+  </nav>
+</template>
+
+<script>
+export default {
+};
+</script>
